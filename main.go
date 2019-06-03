8fea59d0f4a1d7c016773c3add2a32cc0ca42dd2
Am Laher <am@laher.net.nz>
2019-06-03 19:37:20 +1200

    Adding docker-compose support because it's gonna be way easier  * Supporting volumes in parent dirs  * Supporting env-files

--- main.go
+++ main.go
@@ -3,25 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
-	"text/template"
 )
 
 const (
-	dokeFileName     = "Dockerfile.doke"
-	dokeFileTemplate = `FROM alpine 
+	dockerFileName     = "Dockerfile.doke"
+	dockerFileTemplate = `FROM alpine 
 
-RUN mkdir {{.WorkDir}}
-RUN apk add --update make
-
-WORKDIR {{.WorkDir}}
+RUN apk add --update tzdata ca-certificates make
 
 ENTRYPOINT ["make"]
+`
+	dockerComposeFileName = "docker-compose.doke.yaml"
+	dockerComposeTemplate = `version: '3'
+services:
+  make:
+    build:
+    	context: .
+	dockerfile: Dockerfile.doke
+    working_dir: {{ .WorkDir }}
+    volumes:
+      - "$PWD:{{ .WorkDir }}"
+    entrypoint: make
 `
 )
 
@@ -36,6 +45,31 @@ func (i *arrayFlags) Set(value string) error {
 	return nil
 }
 
+func genFileIfNotExist(name, tpl, base string) {
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		fmt.Printf("%s not found - generating ...\n", name)
+		var (
+			t *template.Template
+			w io.WriteCloser
+		)
+		if t, err = template.New("template").Parse(tpl); err != nil {
+			log.Fatal("parsing template: ", err)
+		}
+		if w, err = os.Create(name); err != nil {
+			log.Fatalf("cant write %s: %v", name, err)
+		}
+		if err := t.Execute(w, struct{ WorkDir string }{WorkDir: "/" + base}); err != nil {
+			log.Fatalf("cant write %s: %v", name, err)
+		}
+		if err := w.Close(); err != nil {
+			log.Fatalf("cant write %s: %v", name, err)
+		}
+		fmt.Printf("Wrote a basic %s - please edit it to add your build dependencies\n", name)
+	} else if err != nil {
+		log.Fatalf("cant read filesystem to find %s: %v", name, err)
+	}
+}
+
 func main() {
 	var envVars arrayFlags
 	flag.Var(&envVars, "e", "specify an env var")
@@ -46,44 +80,44 @@ func main() {
 		log.Fatalf("os.Getwd() failed with %s\n", err)
 	}
 	base := filepath.Base(pwd)
-	if _, err := os.Stat(dokeFileName); os.IsNotExist(err) {
-		fmt.Printf("%s not found - generating ...\n", dokeFileName)
-		var (
-			t *template.Template
-			w io.WriteCloser
-		)
-		if t, err = template.New("dockerfile").Parse(dokeFileTemplate); err != nil {
-			log.Fatal("parsing template: ", err)
-		}
-		if w, err = os.Create(dokeFileName); err != nil {
-			log.Fatalf("cant write %s: %v", dokeFileName, err)
-		}
-		if err := t.Execute(w, struct{ WorkDir string }{WorkDir: "/" + base}); err != nil {
-			log.Fatalf("cant write %s: %v", dokeFileName, err)
-		}
-		if err := w.Close(); err != nil {
-			log.Fatalf("cant write %s: %v", dokeFileName, err)
-		}
-		fmt.Printf("Wrote a basic %s - please edit it to add your build dependencies\n", dokeFileName)
-	} else if err != nil {
-		log.Fatalf("cant read filesystem to find %s: %v", dokeFileName, err)
-	}
-	// we are trying to build the equivalent of ...
-	// docker run --rm $(docker build -q -f Dockerfile.doke .) $@
-	cmd := exec.Command("docker", "build", "-q", "-f", "Dockerfile.doke", ".")
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatalf("docker-build failed with %s\n", err)
-	}
-	img := strings.TrimSpace(string(out))
-	// fmt.Println(img) //print output for consistency
 
-	cmd = exec.Command("docker")
-	cmd.Args = append(cmd.Args, "run", "--rm", "-v", fmt.Sprintf("%s:/%s", pwd, base))
-	for _, envVar := range envVars {
-		cmd.Args = append(cmd.Args, "-e", envVar)
+	genFileIfNotExist(dockerFileName, dockerFileTemplate, base)
+	var dockerCompose bool
+	var cmd *exec.Cmd
+	if _, err := exec.LookPath("docker-compose"); err != nil {
+		log.Printf("docker-compose not found: using docker")
+		dockerCompose = false
+		if _, err := exec.LookPath("docker"); err != nil {
+			log.Fatalf("docker not found: %s", err)
+		}
+	} else {
+		dockerCompose = true
 	}
-	cmd.Args = append(cmd.Args, img)
+	if dockerCompose {
+		genFileIfNotExist(dockerComposeFileName, dockerComposeTemplate, base)
+		cmd = exec.Command("docker-compose", "-f", dockerComposeFileName)
+		for _, envVar := range envVars {
+			cmd.Args = append(cmd.Args, "-e", envVar)
+		}
+		cmd.Args = append(cmd.Args, "run", "make")
+	} else {
+		// we are trying to build the equivalent of ...
+		// docker run --rm $(docker build -q -f Dockerfile.doke .) $@
+		cmd = exec.Command("docker", "build", "-q", "-f", dockerFileName, ".")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatalf("docker-build failed with %s\n", err)
+		}
+		img := strings.TrimSpace(string(out))
+		// fmt.Println(img) //print output for consistency
+
+		cmd = exec.Command("docker")
+		cmd.Args = append(cmd.Args, "run", "--rm", "-v", fmt.Sprintf("%s:/%s", pwd, base))
+		for _, envVar := range envVars {
+			cmd.Args = append(cmd.Args, "-e", envVar)
+		}
+		cmd.Args = append(cmd.Args, img)
+	}
 	cmd.Args = append(cmd.Args, os.Args[1:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
